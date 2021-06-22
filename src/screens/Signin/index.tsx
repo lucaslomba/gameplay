2b3ec66c9b30b42da1450b322733a04ad0a6f497
Lucas de Azevedo Godoi Lomba <lucasazevedolomba@gmail.com>
2021-06-21 21:24:51 -0300

    Começando com react native

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, Image, StatusBar} from 'react-native'
+
+import { ButtonIcon } from '../../components/ButtonIcon'
+import IllustrationImg from '../../assets/illustration.png'
+import { styles } from './styles';
+
+export function Signin(){
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+      <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente 
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon title="Entrar com o Discord" activeOpacity={0.8}/>
+      </View>
+    </View>
+  )
+}
