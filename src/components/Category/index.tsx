78488e3b35568efe7308c7bdcc6ab64815b679f8
Lucas de Azevedo Godoi Lomba <lucasazevedolomba@gmail.com>
2021-07-05 18:46:04 -0300

    Ajuste de degrade

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -9,25 +9,29 @@ import { theme } from "../../global/styles/theme";
 type Props = {
     title: string;
     icon: React.FC<SvgProps>;
+    hasCheckBox?: boolean;
     checked?: boolean;
     onPress: () => void;
 }
 
-export function Category({title, icon: Icon, checked=false, ...rest} : Props){
-    const { secondary50, secondary70 } = theme.colors
+export function Category({title, icon: Icon, checked=false, hasCheckBox=true, ...rest} : Props){
+    const { secondary40, secondary50, secondary70, secondary75 } = theme.colors
 
     return (
         <RectButton {...rest}>
             <LinearGradient style={styles.container} colors={[secondary50, secondary70]}>
-                <View style={[styles.content, { opacity: checked ? 1 : 0.4}]}>
-                    <View style={checked ? styles.checked : styles.check} />
+                <LinearGradient style={[styles.content, { opacity: checked ? 1 : 0.5}]} colors={[ checked ? secondary75 : secondary50, secondary40]}>
+                    {
+                        hasCheckBox &&
+                        <View style={checked ? styles.checked : styles.check} />
+                    }
 
                     <Icon width={48} height={48}/>
 
                     <Text style={styles.title}>
                         { title }
                     </Text>
-                </View>
+                </LinearGradient>
             </LinearGradient>
         </RectButton>
     )
